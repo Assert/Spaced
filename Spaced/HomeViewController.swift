94b8ab704f410953dd1bc9ff30f14f765a340493
Eystein Bye <eysteinbye@zedge.net>
2017-12-22 23:52:20 +0100

    Renaming view controllers

---
+++ Spaced/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Spaced
+//
+//  Created by Eystein Bye on 22/12/2017.
+//  Copyright © 2017 Assert. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarController?.delegate = self
+    }
+
+    @IBAction func gotoTasks(_ sender: UIButton) {
+        self.tabBarController?.selectedIndex = 2
+    }
+    
+}
+
