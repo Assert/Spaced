ed60dd93dcf427f5816feceeb2cf2dbd6da32904
Eystein Bye <eysteinbye@zedge.net>
2017-12-23 16:01:30 +0100

    Added writing to Firebase

--- Spaced/AppDelegate.swift
+++ Spaced/AppDelegate.swift
@@ -16,29 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-
-        
+        // Setup Firebase
         FirebaseApp.configure()
 
-        /*
-        
-        let defaultStore = Firestore.firestore()
-        
-        // Add a new document with a generated ID
-        var ref: DocumentReference? = nil
-        ref = db.collection("users").addDocument(data: [
-            "first": "Ada",
-            "last": "Lovelace",
-            "born": 1815
-        ]) { err in
-            if let err = err {
-                print("Error adding document: \(err)")
-            } else {
-                print("Document added with ID: \(ref!.documentID)")
-            }
-        }
-        */
         return true
     }
 
--- Spaced/ViewControllers/TaskViewController.swift
+++ Spaced/ViewControllers/TaskViewController.swift
@@ -7,7 +7,34 @@
 //
 
 import UIKit
+import Firebase
 
-class TaskViewController: NSObject {
+//class TaskViewController: NSObject {
+class TaskViewController: UIViewController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBarController?.delegate = self
+        
+        writeTask()
+    }
+    
+    func writeTask() {
+        let db = Firestore.firestore()
+        
+        // Add a new document with a generated ID
+        var ref: DocumentReference? = nil
+        ref = db.collection("tasks").addDocument(data: [
+            "title": "Napolion",
+            "description": "When did Napolion.....?",
+            "isPublic": false
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+    }
 
 }
